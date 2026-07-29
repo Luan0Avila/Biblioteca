aaf15bfb68a72bdf0f5158928c179eedc3b0f42e
Luan0Avila <avila.almeida.luan@gmail.com>
2026-07-29 13:03:10 -0300

    alterando o modo de escolher opções

--- main.py
+++ main.py
@@ -8,18 +8,33 @@ def mostrar_menu():
     print("6 - Remover livro")
     print("0 - Sair")
 
+rodando = True
 
 def main():
-    while True:
+    while rodando:
         mostrar_menu()
 
         opcao = input("Escolha uma opção: ")
-
-        if opcao == "0":
-            print("Encerrando o sistema...")
-            break
     
         print(f"Você escolheu a opção {opcao}")
+
+        if opcao == "1":
+            pass
+        elif opcao == "2":
+            pass
+        elif opcao == "3":
+            pass
+        elif opcao == "4":
+            pass
+        elif opcao == "5":
+            pass
+        elif opcao == "6":
+            pass
+        elif opcao == "0":
+            rodando = False
+            print("Desligando sistema...")
+        else:
+            print("Opção invalida!")
 
 if __name__=="__main__":
     main()
